c91ecc01f9c8b44a788b576f6e5f7438ac0879f6
HagiSan0927 <hagi.san.09275571@gmail.com>
2023-05-19 01:48:37 +0900

    新規登録、ログイン機能の実装

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
+
 end
