44302e55a58c7b1252e2a2a5fd05e0eaf14db1b7
HagiSan0927 <hagi.san.09275571@gmail.com>
2023-05-20 05:08:56 +0900

    Tailwindcssの設定とログイン、ログアウトページのスタイル整理

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "tops#index"
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
