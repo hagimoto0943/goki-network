95b3530ae8accfe1f7726a5a534f0d14bee77ba6
HagiSan0927 <hagi.san.09275571@gmail.com>
2023-07-16 17:16:34 +0900

    本番環境dbの編集

--- api/bin/render-build.sh
+++ api/bin/render-build.sh
@@ -6,4 +6,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
