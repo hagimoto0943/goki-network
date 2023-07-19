08a6d2913406c0d97bbadfa125ad113d02a8066d
HagiSan0927 <hagi.san.09275571@gmail.com>
2023-07-19 16:37:51 +0900

    ゴキブリ対策投稿のいいね機能対策投稿のいいね機能日ど投稿のいいね機能非同期化

--- api/app/controllers/likes_controller.rb
+++ api/app/controllers/likes_controller.rb
@@ -1,11 +1,13 @@
 class LikesController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
     @post_like = Like.new(user_id: current_user.id, post_id: params[:post_id])
     @post_like.save
     redirect_back fallback_location: posts_path
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     @post_like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
     @post_like.destroy
     redirect_back fallback_location: posts_path
