5038cd437fb4a643540b349a703b64b6191fb107
HagiSan0927 <hagi.san.09275571@gmail.com>
2023-07-24 02:22:53 +0900

    フラッシュメッセージの追加

--- api/app/controllers/profiles_controller.rb
+++ api/app/controllers/profiles_controller.rb
@@ -7,8 +7,9 @@ class ProfilesController < ApplicationController
   def create
     @profile = current_user.build_profile(profile_params)
     if @profile.save
-      redirect_to @profile
+      redirect_to @profile, success: t('.success')
     else
+      flash.now[:error] = t('.fail')
       render :new
     end
   end
