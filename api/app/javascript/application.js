4e2f8dbac23ed1f7b69349b17a818822e70755e9
HagiSan0927 <hagi.san.09275571@gmail.com>
2023-08-01 02:10:45 +0900

    application.jsの名前変更

--- api/app/javascript/application.js
+++ api/app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "controllers";
 import "@hotwired/turbo-rails";
+import "controllers";
 import "@rails/actioncable";
 import "@fortawesome/fontawesome-free";
 import "channels";
