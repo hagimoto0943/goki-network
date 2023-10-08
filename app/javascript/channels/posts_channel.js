f20f0973a5b8c6b85bdc6537837632b0fb970594
HagiSan0927 <hagi.san.09275571@gmail.com>
2023-10-08 20:23:58 +0900

    退治開始機能の表示変更

--- app/javascript/channels/posts_channel.js
+++ app/javascript/channels/posts_channel.js
@@ -12,18 +12,18 @@ consumer.subscriptions.create("PostsChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    const messageName = data.name;
+    const name = data.name;
+    const date = data.created_at;
     const message = `
-        <div class="items-center w-full max-w-xs p-4 animate-pulse text-gray-500 bg-white rounded-lg shadow role="alert">
-          <div class="ml-3 text-sm font-normal">${messageName}さんが退治を開始しました。</div>
-        </div>`;
-    const alertMessage = document.getElementById("alert");
+    <button class="flex items-center w-full px-5 py-2 transition-colors duration-200 gap-x-2 ">
+      <i class="fa-solid fa-user w-8 h-8 rounded-full p-2 bg-neutral-400"></i>
+        <div class="text-left rtl:text-right">
+            <h1 class="text-sm font-medium text-gray-700 capitalize">${name}</h1>
+            <p class="text-xs text-gray-500 "><%= l fight.user.created_at %>${date}</p>
+        </div>
+    </button>
+        `;
+    const alertMessage = document.getElementById("sideber");
     alertMessage.insertAdjacentHTML("afterbegin", message);
-    function removeAlert() {
-      alertMessage.classList.add("hidden");
-    }
-    setTimeout(() => {
-      removeAlert();
-    }, 5000);
   },
 });
