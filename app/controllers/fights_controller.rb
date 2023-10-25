44bdecf2f4ad3c7efeb1d1ece566b490d8a02140
HagiSan0927 <hagi.san.09275571@gmail.com>
2023-10-25 13:25:56 +0900

    fights_controllerのリファクタリング

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -1,10 +1,11 @@
 class FightsController < ApplicationController
+  before_action :set_status, only: %i[create update]
+
   def index
     @fights = Fight.includes(:user).order(created_at: :desc).limit(10)
   end
 
   def create
-    @fights_progress = Fight.not_done
     @fight = current_user.fights.new
     if @fight.save
       ActionCable.server.broadcast(
@@ -21,7 +22,6 @@ class FightsController < ApplicationController
 
   def update
     @fight = current_user.fights.find_by(status: 1)
-    @fights_progress = Fight.not_done
     if @fight.toggle_status!
       ActionCable.server.broadcast(
         'some_channel', {
@@ -33,5 +33,11 @@ class FightsController < ApplicationController
       flash.now[:error] = t('.fail')
       render :index
     end
+  end
+
+  private
+
+  def set_status
+    @fights_progress = Fight.not_done
   end
 end
