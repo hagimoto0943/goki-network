5702f9974f2278603fa02098f74f823ab99ab37d
HagiSan0927 <hagi.san.09275571@gmail.com>
2023-10-27 23:38:47 +0900

    fightのmodelspec記述

--- spec/models/fight_spec.rb
+++ spec/models/fight_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Fight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#create" do
+    it "statusはprogressのみ登録できる" do
+      fight = build(:fight, status: 0)
+      fight.valid?
+    end
+  end
 end
