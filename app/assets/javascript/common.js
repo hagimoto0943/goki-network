540848aa4c643718074a79302d593cfd71edebd2
HagiSan0927 <hagi.san.09275571@gmail.com>
2023-12-06 12:24:19 +0900

    like_buttonの編集

--- app/assets/javascript/common.js
+++ app/assets/javascript/common.js
@@ -1,14 +1,14 @@
 function previewImage() {
-  const target = this.event.target;
-  const file = target.files[0];
-  const reader = new FileReader();
-  reader.onloadend = function () {
-    const preview = document.querySelector("#preview");
-    if (preview) {
-      preview.src = reader.result;
+    const target = this.event.target;
+    const file = target.files[0];
+    const reader  = new FileReader();
+    reader.onloadend = function () {
+        const preview = document.querySelector("#preview")
+        if(preview) {
+            preview.src = reader.result;
+        }
     }
-  };
-  if (file) {
-    reader.readAsDataURL(file);
+    if (file) {
+        reader.readAsDataURL(file);
+    }
   }
-}
